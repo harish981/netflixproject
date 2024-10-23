fc22883dea3adf0671ff5649f63dccfa8a502bd3
harish981 <113511497+harish981@users.noreply.github.com>
2024-10-23 22:10:45 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+count = 4
+ami = "ami-0ddc798b3f1a5117e"
+instance_type = "t2.medium"
+key_name = "mkp"
+vpc_security_group_ids = ["sg-06f881f5585b6cae8"]
+tags = {
+Name = var.instance_names[count.index]
+}
+}
+
+variable "instances_names" {
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2"]
+}
